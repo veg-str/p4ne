88b3ef64f724b18fa2ee670f720ac4f27d475e15
veg-str <veg_str@outlook.com>
2018-03-23 14:10:27 +0300

    First commit

--- Lab1.5/strings_and_files.py
+++ Lab1.5/strings_and_files.py
@@ -3,9 +3,17 @@ import os
 dir = 'c:\\Users\\ve.gusarin\\Seafile\\p4ne_training\\config_files\\'
 files_list = os.listdir(dir)
 os.chdir(dir)
+ip = []
+
 
 for x in files_list:
     with open(x) as f:
         for l in f:
-            if l.strip().startswith('ip address'): print(l)
+            if l.strip().startswith('ip address'):
+                y = l.lstrip('ip address ').rstrip()
+                ip.append(y)
 
+
+
+ip = list(set(ip))
+print(ip)
