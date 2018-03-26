1e5d83f4ede332ba28f9f9ddda441951133ddc21
veg-str <veg_str@outlook.com>
2018-03-26 18:33:24 +0300

    First commit

--- HomeWork/ipplan.py
+++ HomeWork/ipplan.py
@@ -1,22 +1,42 @@
 import ipaddress, openpyxl, re, os
 
-
 dir = 'c:\\Users\\ve.gusarin\\Seafile\\p4ne_training\\config_files\\'
 files_list = os.listdir(dir)
 os.chdir(dir)
 
+ip = []
+ipplan_item = {}
+ipplan = []
+
+
 def classify(s):
     if re.search("ip address ((?:[0-9]{1,3}\.?){4}) ((?:[0-9]{1,3}\.?){4})", s):
         x = re.match(" ip address ((?:[0-9]{1,3}\.?){4}) ((?:[0-9]{1,3}\.?){4})", s)
-        return {'ip': ipaddress.IPv4Interface(str(x.group(1)) + "/" + str(x.group(2))).with_netmask}
+        return str.strip(x.group(1)) + "/" + str.strip(x.group(2))
     else:
-        return {}
+        return False
 
-ip = []
+
 for x in files_list:
     with open(x) as f:
         for l in f:
-            dict = classify(l)
-            if 'ip' in dict: ip.append(dict)
+            net = classify(l)
+            if net != False: ip.append(ipaddress.IPv4Interface(net))
 
-print(ip)
+ip = sorted(list(set(ip)))
+
+for i in ip:
+    ipplan.append({'net':str(i.network.network_address), 'mask':str(i.netmask), 'gw':str(i.ip)})
+
+
+print('{:<30}'.format('Сеть'), '{:<30}'.format('Маска'), '{:<30}'.format('Шлюз'))
+for i in ipplan:
+    print('{:<30}'.format(i.get('net')), '{:<30}'.format(i.get('mask')), '{:<30}'.format(i.get('gw')))
+
+wb = openpyxl.Workbook()
+ws = wb.active
+ws.title = 'IP Plan'
+
+
+
+
